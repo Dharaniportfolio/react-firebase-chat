05a561e0bfb9cad3add3d9e687a107a77ce688bb
Dharaniportfolio <mdharani228@gmail.com>
2024-09-09 22:02:34 +0530

    fix:make changes in login with signin

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import Detail from "./components/details/detail"
 import List from "./components/list/list"
 import Login from "./components/login/login"
 import Notification from "./components/notification/notification"
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase"
+import { useEffect } from "react";
 const App = () => {
   const user = false;
+  useEffect(() =>{
+    const unSub = onAuthStateChanged(auth,(user)=>{
+      console.log(user)
+    });
+    return () =>{
+      unSub();
+    }
+  },[])
   return (
     <div className='container'>
       {user ? (
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./login.css";
 import { toast } from "react-toastify";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword ,signInWithEmailAndPassword} from "firebase/auth";
 import {auth,db} from "../../lib/firebase"
 import { doc, setDoc } from "firebase/firestore";
 import upload from "../../lib/upload";
@@ -13,6 +13,8 @@ function Login(){
         url:""
 
     })
+    
+    const [loading, setLoading] = useState(false);
 
     const handleavatar = e =>{
         if(e.target.files[0]){
@@ -23,12 +25,24 @@ function Login(){
     }
     }
     
-    const handlelogin = e =>{
+    const handlelogin = async(e) =>{
         e.preventDefault()
+        setLoading(true);
+        const formdata = new FormData(e.target);
+        const {email,password} = Object.fromEntries(formdata);
+        try{
+            await signInWithEmailAndPassword(auth,email,password)
+        }catch(err){
+            console.log(err)
+            toast.error(err.message);
+        }finally{
+            setLoading(false);
+        }
     }
 
     const handleRegister = async (e) =>{
         e.preventDefault()
+        setLoading(true)
         const formdata = new FormData(e.target);
         const {username, email,password} = Object.fromEntries(formdata);
         try{
@@ -51,6 +65,8 @@ function Login(){
         catch(err){
             console.log(err);
             toast.error(err.message);
+        }finally{
+            setLoading(false)
         }
     }
     return(
@@ -60,7 +76,7 @@ function Login(){
             <form onSubmit={handlelogin}>
                 <input type="text" placeholder="Email" name="email" />
                 <input type="password" placeholder="Password" name="password" />
-                <button>Sign In</button>
+                <button disabled={loading}>{loading ? "Loading":"Sign In"}</button>
             </form>
             </div>
             <div className="separator"></div>
@@ -74,7 +90,7 @@ function Login(){
                 <input type="text" placeholder="Username" name="username" />
                 <input type="text" placeholder="Email" name="email" />
                 <input type="password" placeholder="Password" name="password" />
-                <button>Sign up</button>
+                <button disabled={loading}>{loading ? "Loading":"Sign up"}</button>
             </form>
             </div>
         </div>
